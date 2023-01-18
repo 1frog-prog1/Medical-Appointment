36a31e678cc1eacbd20ca0fd5d0217fa89621280
Margarita Kikot <ritkaplitka2003@mail.ru>
2023-01-18 23:55:01 +1000

    add addDoctorAppintment tests

--- domain/models/sheldue/SheldueUsecases.cs
+++ domain/models/sheldue/SheldueUsecases.cs
@@ -24,14 +24,22 @@ namespace domain.models.sheldue
         }
 
         public Result<Appointment> addDoctorAppointment(Appointment appointment) {
+            var end_day = new DateTime(1, 1, 1, 18, 0, 0).TimeOfDay;
+            if(appointment.doctor_id <= 0)
+                return Result.Fail<Appointment>("The doctor_id is negative");
+
             if (!doctorRepository.isExist(appointment.doctor_id))
                 return Result.Fail<Appointment>("Such doctor doesn't exist");
-            if (repository.isDoctorTimeIsBusy(appointment.doctor_id, appointment.start));
+            
+            if (appointment.start.TimeOfDay > end_day)
+                return Result.Fail<Appointment>("Such time doesn't exist");
+
+
+            if (repository.isDoctorTimeIsBusy(appointment.doctor_id, appointment.start))
                 return Result.Fail<Appointment>("The appointment time is already busy");
 
             repository.addDoctorAppointment(appointment);
             return Result.Ok<Appointment>(appointment);
-            
         }
     }
 }
--- tests/SheldueUsecasesTests.cs
+++ tests/SheldueUsecasesTests.cs
@@ -5,6 +5,7 @@ using System;
 
 using domain.models.sheldue;
 using domain.models.doctor;
+using domain.models;
 
 namespace tests
 {
@@ -64,5 +65,82 @@ namespace tests
             Assert.True(res.Success);
             Assert.Equal(res.Error, "");
         }
+
+        [Fact]
+        public void addDoctorAppointmentByNegativeDoctorId_Fail()
+        {
+            // Given
+            Appointment app = new Appointment(new DateTime(1, 1, 1, 14, 20, 0), 1, 0);
+        
+            // When
+            var res = usecases.addDoctorAppointment(app);
+        
+            // Then
+            Assert.False(res.Success);
+            Assert.Equal(res.Error, "The doctor_id is negative");
+        }
+
+        [Fact]
+        public void addDoctorAppointmentByIncorrectDoctorId_Fail()
+        {
+            // Given
+            Appointment app = new Appointment(new DateTime(1, 1, 1, 14, 20, 0), 1, 1);
+            doctorRepository.Setup(rep => rep.isExist(app.doctor_id)).Returns(false);
+        
+            // When
+            var res = usecases.addDoctorAppointment(app);
+        
+            // Then
+            Assert.False(res.Success);
+            Assert.Equal(res.Error, "Such doctor doesn't exist");
+        }
+
+        [Fact]
+        public void addDoctorAppointmentByIncorrectTime_Fail()
+        {
+            // Given
+            Appointment app = new Appointment(new DateTime(1, 1, 1, 18, 20, 0), 1, 1);
+            doctorRepository.Setup(rep => rep.isExist(app.doctor_id)).Returns(true);
+        
+            // When
+            var res = usecases.addDoctorAppointment(app);
+        
+            // Then
+            Assert.False(res.Success);
+            Assert.Equal(res.Error, "Such time doesn't exist");
+        }
+
+        [Fact]
+        public void addDoctorAppointmentByBusyTime_Fail()
+        {
+            // Given
+            Appointment app = new Appointment(new DateTime(1, 1, 1, 17, 20, 0), 1, 1);
+            doctorRepository.Setup(rep => rep.isExist(app.doctor_id)).Returns(true);
+            repository.Setup(rep => rep.isDoctorTimeIsBusy(app.doctor_id, app.start)).Returns(true);
+        
+            // When
+            var res = usecases.addDoctorAppointment(app);
+        
+            // Then
+            Assert.False(res.Success);
+            Assert.Equal(res.Error, "The appointment time is already busy");
+        }
+
+        [Fact]
+        public void addDoctorAppointmentCorrectData_Ok()
+        {
+            // Given
+            Appointment app = new Appointment(new DateTime(1, 1, 1, 17, 20, 0), 1, 1);
+            doctorRepository.Setup(rep => rep.isExist(app.doctor_id)).Returns(true);
+            repository.Setup(rep => rep.isDoctorTimeIsBusy(app.doctor_id, app.start)).Returns(false);
+        
+            // When
+            var res = usecases.addDoctorAppointment(app);
+        
+            // Then
+            Assert.True(res.Success);
+            Assert.Equal(res.Error, "");
+        }
+
     }
 }
