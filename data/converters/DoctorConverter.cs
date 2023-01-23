2d7bf464b8e20443f2292f3efd971dc17a5a7dca
Margarita Kikot <ritkaplitka2003@mail.ru>
2023-01-23 15:49:49 +1000

    add DoctorConverter

--- data/converters/DoctorConverter.cs
+++ data/converters/DoctorConverter.cs
@@ -1,7 +1,24 @@
+using domain.models;
+using data.models;
+
 namespace data.converters
 {
     public class DoctorConverter
     {
-        
+        public static Doctor? toDomain(DoctorModel model) {
+            return new Doctor (
+                model.Id,
+                model.fio,
+                model.specialisation_id
+            );
+        }
+
+    public static DoctorModel toModel(Doctor user) {
+        return new DoctorModel (
+            user.Id,
+            user.fio,
+            user.specialisation_id
+        );
+    }
     }
 }
--- domain/models/doctor/Doctor.cs
+++ domain/models/doctor/Doctor.cs
@@ -6,6 +6,12 @@ namespace domain.models
         public string fio;
         public int specialisation_id;
 
+        public Doctor(int Id, string fio, int specialisation_id) {
+            this.Id = Id;
+            this.fio = fio;
+            this.specialisation_id = specialisation_id;
+        }
+
         public Doctor(string fio, int specialisation_id) {
             this.fio = fio;
             this.specialisation_id = specialisation_id;
